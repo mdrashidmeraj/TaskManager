9a6f254bab669aa8429a3931e5800b203f7dbd05
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2023-04-10 01:53:14 +0530

    login

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api-2', {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false
--- src/models/user.js
+++ src/models/user.js
@@ -13,6 +13,7 @@ const UserSchema = new mongoose.Schema({
         required: true,
         trim: true,
         lowercase: true,
+        unique: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
@@ -41,6 +42,18 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
+UserSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+    if(!user){
+        throw new Error('Unable to login');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        throw new Error('Unable to login');
+    }
+    return user;
+}
+    
 UserSchema.pre('save', async function (next) {
     const user = this;
     if(user.isModified('password')){
