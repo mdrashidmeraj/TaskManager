82cb1e13240c4c5ffbe73195186fae4fb472bee5
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2023-04-21 03:46:01 +0530

    mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false
